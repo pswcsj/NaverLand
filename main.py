34adf827395c277dd03ab1eda89498ffd3f9d0c3
a <pswxsj@naver.com>
2021-12-19 20:31:13 +0900

    code shorter

--- main.py
+++ main.py
@@ -31,64 +31,46 @@ gun_dict = {
     '사무실':'SMS'
 }
 
-
 driver = webdriver.Chrome('./chromedriver')
-driver.get(url='https://m.land.naver.com')
-time.sleep(0.1)
-driver.find_element(By.XPATH, '//*[@id="ct"]/div/div[2]/div/div[2]/div[2]/div[2]/a').click()
-time.sleep(0.1)
 
-# 시/동 선택
-lists = []
-while not lists:
-    lists = driver.find_elements(By.CSS_SELECTOR, ".region_inner._region_item")
-    loc_f_list = [list.text for list in lists]
+loc = input("지역을 입력하세요 > ")
+driver.get('https://m.land.naver.com/search/result/'+loc)
+while driver.current_url.split('/')[3] != 'map':
+    loc = input("지역을 더 자세히 입력하세요 > ")
+    driver.get('https://m.land.naver.com/search/result/' + loc)
     time.sleep(0.1)
-print(loc_f_list)
-loc_f = input("위에 제시된 목록 중 거래 지역을 입력하세요 >")
-lists[loc_f_list.index(loc_f)].click()
-time.sleep(1)
-
-# 시/군/구 선택
-lists = []
-while not lists:
-    lists = driver.find_elements(By.CSS_SELECTOR, ".region_inner._region_item")
-    loc_s_list = [list.text for list in lists]
-    time.sleep(0.1)
-# loc_p = [text if text != '' else None for text in loc_s_list]
-print(loc_s_list)
-loc_s = input("위에 제시된 목록 중 거래 지역을 입력하세요 >")
-lists[loc_s_list.index(loc_s)].click()
-time.sleep(1)
-
-# 읍/면/동 선택
-choice = input("읍/면/동까지 입력하시겠습니까?(O/X)")
-if (choice == 'o') | (choice == 'O'):
-    lists = []
-    while not lists:
-        lists = driver.find_elements(By.CSS_SELECTOR, ".region_inner._region_item")
-        loc_t_list = [list.text for list in lists]
-        time.sleep(0.1)
-    print(loc_t_list)
-    loc_t = input("위에 제시된 목록 중 거래 지역을 입력하세요 >")
-    lists[loc_t_list.index(loc_t)].click()
-
-#검색 클릭
-try: driver.find_element(By.XPATH, '//*[@id="mapSearch"]/div[2]/div[1]/section/div[2]/a').click()
-except: time.sleep(0.1)
-time.sleep(1)
 
 #url로 조건 설정
 url_divide = driver.current_url.split('/'); url_divide.pop(); url_divide.pop();
 url = '/'.join(url_divide)
 print(list(type_dict.keys()))
-type = type_dict[input("원하는 거래 방식을 입력하세요 >")]
+type = input("원하는 거래 방식을 입력하세요 >")
+type_key = type_dict[type]
+
 print(list(gun_dict.keys()))
 gun = gun_dict[input("원하는 건물을 입력하세요 >")]
-driver.get(url+'/'+gun+'/'+type)
+
+if type=='매매':
+    priceMin = input("매매가의 최솟값을 입력하세요(만) >")
+    priceMax = input("매매가의 최댓값을 입력하세요(만) >")
+    price = '?dprcMin='+str(priceMin)+'&dprcMax='+str(priceMax)+'&'
+elif type=='전세':
+    priceMin = input("보증금의 최솟값을 입력하세요(만) >")
+    priceMax = input("보증금의 최댓값을 입력하세요(만) >")
+    price = '?wprcMin='+str(priceMin)+'&wprcMax='+str(priceMax)+'&'
+elif (type=='월세') | (type=='단기임대'):
+    priceWMin = input("보증금의 최솟값을 입력하세요(만) >")
+    priceWMax = input("보증금의 최댓값을 입력하세요(만) >")
+    priceMMin = input("월세의 최솟값을 입력하세요(만) >")
+    priceMMax = input("월세의 최댓값을 입력하세요(만) >")
+    price = '?wprcMin='+str(priceWMin)+'&wprcMax='+str(priceWMax)+\
+            '&rprcMin='+str(priceMMin)+'&rprcMax='+str(priceMMax)+'&'
+
+driver.get(url+'/'+gun+'/'+type_key+price)
+print(driver.current_url)
 time.sleep(1)
 
-
+# https://m.land.naver.com/cluster/clusterList?view=atcl&cortarNo=4127300000&rletTpCd=OPST&tradTpCd=A1%3AB1%3AB2&z=13&lat=37.319537&lon=126.811832&pCortarNo=%20Request%20Method:%20GET
 
 #매물 보기 클릭
 try: driver.find_element(By.XPATH, '//*[@id="_mapSection"]/div[2]').click()
